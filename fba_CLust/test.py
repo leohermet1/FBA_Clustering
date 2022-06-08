09aa27e91a23cc4d1a0f9efab7ccba89f875430c
Leo Hermet <lhermet@nnfcb-l0373.clients.net.dtu.dk>
2022-06-08 13:38:43 +0200

    remove test function

--- fba_CLust/test.py
+++
@@ -1,8 +0,0 @@
-import scipy.io as spio
-
-#load the matlab structure
-mean10 = spio.loadmat('/home/leo/test_data/modelPatient_Sampled_10_Mean.mat')
-
-#get the structure array
-sample10mean = mean10['sampleMetaOutC']
-print(sample10mean.dtype)
