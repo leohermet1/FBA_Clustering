25f3a281787513e5d3845232ccf92ccf1c7e6ceb
Leo Hermet <lhermet@nnfcb-l0373.clients.net.dtu.dk>
2022-06-09 15:21:08 +0200

    add questions.py

--- fba_CLust/main.py
+++ fba_CLust/main.py
@@ -1,29 +1,31 @@
+import questions as q
 import preProc as pp
-import tensorDec as td
 import loadNorm as l
-import sys 
+import tensorDec as td
 
-if len(sys.argv) == 1:
-    print(TypeError: main() missing 1 required positional argument: 'argv')
+
+bool = q.npyQuestion()
+
+if bool:
+    Nm = q.npyPath()
+    l.getNm(Nm)
 
 else:
-    if len(sys.argv) == 2:
-    #get the paths of all the .mat files
-    paths = pp.getPaths(sys.argv[1])
+    matDir = q.matPath()
 
-    #get only the paths of the .mat files with
-    #the solution matrices computed from phenotypes
-    #with boudaries compared to the mean of the healthy patients
+    # get the paths of all the .mat files
+    paths = pp.getPaths(matDir)
+
+    # get only the paths of the .mat files with
+    # the solution matrices computed from phenotypes
+    # with boudaries compared to the mean of the healthy patients
     pathsM = pp.getMeanP(paths)
 
-    #get the normalized matrices of each patients 
+    # get the normalized matrices of each patients
     Nm = pp.getNormM(pathsM)
     print(Nm.shape)
 
-if len(sys.argv) == 3:
-    Nm = sys.argv[2]
-    l.getNm(Nm)
-
 cp = td.getCPdecomposition(Nm,2)
 print(td.getReconstructionError(cp,Nm))
+
 
---
+++ fba_CLust/questions.py
@@ -0,0 +1,44 @@
+def npyQuestion():
+    i = 0
+    while i < 2:
+        npy = input("Do you have a .npy matrix? (yes or no)")
+        if any(npy.lower() == f for f in ["yes", 'y', '1', 'ye']):
+            return(True)
+            break
+        elif any(npy.lower() == f for f in ['no', 'n', '0']):
+            return(False)
+            break
+        else:
+            i += 1
+            if i < 2:
+                print('Please enter yes or no')
+            else:
+                print("Nothing done")
+
+def npyPath():
+    i = 0
+    while i < 2:
+        npyP = input("Path of the .npy file:")
+        if len(npyP)>0:
+            return (npyP)
+            break
+        else:
+            i += 1
+            if i < 2:
+                print('Please enter a path')
+            else:
+                print("Nothing done")
+
+def matPath():
+    i = 0
+    while i < 2:
+        matP = input("Path of the directory with all the .mat files:")
+        if len(matP)>0:
+            return (matP)
+            break
+        else:
+            i += 1
+            if i < 2:
+                print('Please enter a path directory')
+            else:
+                print("Nothing done")
