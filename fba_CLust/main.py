6a35e9e967cda545a694a9a880058faaef149a9b
leo hermet <leo.hrmet@gmail.com>
2022-06-09 14:42:01 +0200

    update main.py

--- fba_CLust/main.py
+++ fba_CLust/main.py
@@ -4,8 +4,12 @@ import loadNorm as l
 import sys 
 
 if len(sys.argv) == 1:
+    print(TypeError: main() missing 1 required positional argument: 'argv')
+
+else:
+    if len(sys.argv) == 2:
     #get the paths of all the .mat files
-    paths = pp.getPaths('/home/leo/data/')
+    paths = pp.getPaths(sys.argv[1])
 
     #get only the paths of the .mat files with
     #the solution matrices computed from phenotypes
@@ -13,13 +17,13 @@ if len(sys.argv) == 1:
     pathsM = pp.getMeanP(paths)
 
     #get the normalized matrices of each patients 
-    norms = pp.getNormM(pathsM)
-    print(norms.shape)
+    Nm = pp.getNormM(pathsM)
+    print(Nm.shape)
 
-if len(sys.argv) != 1:
-    Nm = sys.argv[1]
+if len(sys.argv) == 3:
+    Nm = sys.argv[2]
     l.getNm(Nm)
 
-#cp = td.getCPdecomposition(norms,2)
-#print(td.getReconstructionError(cp,norms))
+cp = td.getCPdecomposition(Nm,2)
+print(td.getReconstructionError(cp,Nm))
 
