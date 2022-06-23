63b0224e23a633d3a70befc2e9f4bd618ca1b6a4
Leo Hermet <lhermet@nnfcb-l0373.clients.net.dtu.dk>
2022-06-23 20:14:17 +0200

    modify return main for bash

--- bashT/main.py
+++ bashT/main.py
@@ -5,7 +5,7 @@ import tensorDec as td
 
 Nm = l.getNm('/zhome/5e/6/176978/Desktop/FBA_Clustering/bashT/test_data/tensor95.npy')
 
-lComp = [1]
+lComp = [2,4,10,20,40,80]
 rec_error_cp, cp_time, rec_error_par2, par2_time = td.rec_error_cpANDparafac2(Nm,lComp)
 print('rec_error_cp: ',rec_error_cp,
       '\ncp_time: ',cp_time,
