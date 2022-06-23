76f50def47ef5cfc859b9fe391880c175cd2203e
Leo Hermet <lhermet@nnfcb-l0373.clients.net.dtu.dk>
2022-06-23 20:04:51 +0200

    modify return main for bash

--- bashT/main.py
+++ bashT/main.py
@@ -7,6 +7,6 @@ Nm = l.getNm('/zhome/5e/6/176978/Desktop/FBA_Clustering/bashT/test_data/tensor95
 
 lComp = [1,2]
 rec_error_cp, cp_time, rec_error_par2, par2_time = td.rec_error_cpANDparafac2(Nm,lComp)
-print(rec_error_cp)
+print(rec_error_cp, cp_time, rec_error_par2, par2_time)
 
 
