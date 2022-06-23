87de239548b8b23b74faa81c5314604e609d46bc
Leo Hermet <lhermet@nnfcb-l0373.clients.net.dtu.dk>
2022-06-23 18:09:02 +0200

    update bashscript for hpc

--- bashT/main.py
+++ bashT/main.py
@@ -1,15 +1,9 @@
-import fileinput
 import preProc as pp
 import loadNorm as l
 import tensorDec as td
 
-i=0
-for line in fileinput.input(files='input.in'):
-    if i==0:
-        numpyPath = line
-    i += 1
 
-Nm = l.getNm(numpyPath)
+Nm = l.getNm('/zhome/5e/6/176978/Desktop/FBA_Clustering/bashT/test_data/tensor.npy')
 
 lComp = [1,2]
 rec_error_cp, cp_time, rec_error_par2, par2_time = td.rec_error_cpANDparafac2(Nm,lComp)
