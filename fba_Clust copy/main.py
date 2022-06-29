4d185ee3e80289571408c8a18998d2e85d9a60f0
Leo Hermet <lhermet@nnfcb-l0373.clients.net.dtu.dk>
2022-06-29 17:30:43 +0200

    rm testdata size file

---
+++ fba_Clust copy/main.py
@@ -0,0 +1,38 @@
+import questions as q
+import preProc as pp
+import loadNorm as l
+import tensorDec as td
+
+
+boolNPY = q.npyQuestion()
+
+if boolNPY:
+    numpyPath = q.npyPath()
+    Nm = l.getNm(numpyPath)
+
+else:
+    matDir = q.matPath()
+
+    # get the paths of all the .mat files
+    paths = pp.getPaths(matDir)
+
+    # get only the paths of the .mat files with
+    # the solution matrices computed from phenotypes
+    # with boudaries compared to the mean of the healthy patients
+    pathsM = pp.getMeanP(paths)
+
+    boolSVD = q.SVDquestion()
+    if boolSVD:
+        nb_components = q.nbComp()
+        Nm = pp.getSVD(pathsM,nb_components)
+        print(Nm.shape)
+    else:
+        # get the normalized matrices of each patients
+        Nm = pp.getNormM(pathsM)
+        print(Nm.shape)
+
+lComp = [1,2]
+rec_error_cp, cp_time, rec_error_par2, par2_time = td.rec_error_cpANDparafac2(Nm,lComp)
+print(rec_error_cp)
+
+
---
+++ fba_Clust copy/preProc.py
@@ -0,0 +1,153 @@
+### imports ###
+
+import os
+
+import scipy.io as spio
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import TruncatedSVD
+
+import numpy as np
+
+import re
+
+import questions as q
+
+
+
+### Get the paths ###
+
+#definition of the function that get all the paths of the .mat files
+def getPaths(directory):
+    p = []
+    #browse every files in the directory
+    for filename in os.listdir(directory):
+        #only get the files that ends by '.mat'
+        if filename.endswith(".mat") :
+            p.append(os.path.join(directory, filename))
+            continue
+        else:
+            continue
+    return p
+
+#definition of the function that get all the paths of the Mean.mat files
+def getMeanP(paths):
+    mP = []
+    for p in paths:
+        if p[-8:] == 'Mean.mat':
+            mP.append(p)
+    return mP
+
+# definition of the function that get the solution-reaciton matrices of a patient from the path of a .mat file
+def getSol(path):
+    # load the matlab structure
+    load = spio.loadmat(path)
+
+    # get the structure array
+    structure = load['sampleMetaOutC']
+
+    # get solution space matrix
+    points = structure['points'][0, 0]  # row = reactions, col = solutions
+    return points
+
+
+
+### Normalisation and reduce the dimensions using SVD ###
+
+
+# definition of the function that normalize the matrices and compute SVD
+def normSVD(matrix, nb_components):
+    # normalize
+    matrixT = matrix.transpose()
+    zscore = StandardScaler().fit(matrixT)
+    X_zT = zscore.transform(matrixT)
+    X_z = X_zT.transpose()
+    # SVD 100 component
+    svd = TruncatedSVD(n_components=nb_components)
+    X_svd = svd.fit_transform(X_z)
+
+    # show the variance explained
+    var_explained = svd.explained_variance_ratio_.sum()
+    print(nb_components, 'components explain', round(var_explained * 100, 2), "% of the variance")
+
+    return X_svd
+
+#definition of the function that compute the SVD with 1 component from multiple .mat files
+def getSVD(paths,nb_components):
+    #create the dictionary for the svd matrices of each patients
+    patients = {}
+    nP = []
+    #browse every paths gived in arguments
+    for pm in paths:
+        #get the solution matrice of the patient
+        p = getSol(pm)
+        #get the ID
+        pID = int(re.search(r'\d+', pm).group())
+        nP.append("p{0}".format(pID))
+        print('Patient',pID,':')
+        #get the svd matrix
+        p_svd = normSVD(p,nb_components)
+        #adding patient ID as key and svd as item for the dictionary
+        patients["p{0}".format(pID)] = p_svd
+
+    # get all the normalized matrices in one using stack and a generator function
+    norms = np.stack([patients[nP[i]] for i in range(len(patients))])
+
+    #ask if you want to save the preprocessed data as a .npy file
+    bool = q.npySave()
+    if bool:
+        # get the path where .npy file will be saved
+        newNPYfileP = q.npySavePath()
+        # save the tensor as .npy file if asked
+        np.save(newNPYfileP, norms)
+
+    return norms
+
+
+
+### Normalisation ONLY ###
+
+
+# definition of the function that normalize the matrices and compute SVD with 1 component
+def norm(matrix):
+    # normalize
+    matrixT = matrix.transpose()
+    zscore = StandardScaler().fit(matrixT)
+    X_zT = zscore.transform(matrixT)
+    X_z = X_zT.transpose()
+
+    return X_z
+
+#definition of the function that compute the SVD with 1 component from multiple .mat files
+def getNormM(paths):
+    #create the dictionary for the svd matrices of each patients
+    patients = {}
+    #browse every paths gived in arguments
+    for pm in paths:
+        #get the solution matrice of the patient
+        p = getSol(pm)
+        #get the ID
+        if pm[-11:-9].find('_') == 0:       #if there is a '_' in pm[-11:-9] so if the patient number <10
+            pID = pm[-10:-9]
+        #if pm[-11:-9].find('_') == -1
+        else:
+            pID = pm[-11:-9]
+        nP.append("p{0}".format(pID))
+        print('Patient',pID)
+        #get the normalized matrix
+        pz = norm(p)
+            #adding patient ID as key and svd as item for the dictionary
+        patients["p{0}".format(pID)] = pz
+        
+    # get all the normalized matrices in one using stack and a generator function
+    norms = np.stack((patients[nP[i]] for i in range(len(patients))))
+
+    #ask if you want to save the preprocessed data as a .npy file
+    bool = q.npySave()
+    if bool:
+        # get the path where .npy file will be saved 
+        newNPYfileP = q.npySavePath()
+        # save the tensor as .npy file if asked
+        np.save(newNPYfileP, norms)
+
+    return norms
+
